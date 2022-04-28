9d854692013a7741291b6054a603f0ea15fbab3f
Martin <public@e-mis.cz>
2022-04-28 19:52:26 +0200

    Vylepšení pozdravu
    Doplněno "how are You?"

--- ukazka/src/com/engeto/lekce04/Main.java
+++ ukazka/src/com/engeto/lekce04/Main.java
@@ -3,6 +3,6 @@ package com.engeto.lekce04;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git, how are You?");
     }
 }
